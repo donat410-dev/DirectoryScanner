fd64e036208f465fa8c98ca5c9b2d075cf5ef52e
Alebarda410 <donat410@gmail.com>
2023-06-19 17:25:46 +0300

    minor fix

--- DirectoryScanner/Program.cs
+++ DirectoryScanner/Program.cs
@@ -9,11 +9,11 @@ namespace DirectoryScanner
         {
             Console.WriteLine("Начато сканирование директории");
             var tree = FileSystemElement.GenTree(Directory.GetCurrentDirectory());
-            Console.WriteLine($"Cканирование директории завершено");
+            Console.WriteLine("Cканирование директории завершено");
 
             Console.WriteLine("Начато построение html документа");
             BuildHtml.Start(tree, true);
-            Console.WriteLine($"Построение html документа завершено");
+            Console.WriteLine("Построение html документа завершено");
             Console.ReadKey();
         }
     }
